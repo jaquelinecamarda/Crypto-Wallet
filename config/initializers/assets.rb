4a1466ca60e70cbf8b04f83f8683cc55488caa5e
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-10-24 17:45:12 -0300

    utilizando asset css+erb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -11,4 +13,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w[ cable.js
+                                                  coins.js
+                                                  mining_types.js
+                                                  welcome.js]
+
+Rails.application.config.assets.precompile += %w[ coins.css
+                                                  mining_types.css
+                                                  scaffolds.css
+                                                  welcome.css]
